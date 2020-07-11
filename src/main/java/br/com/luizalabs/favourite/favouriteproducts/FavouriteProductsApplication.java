7387dc1a5a83fce41325049e762bcf23b21edec6
unknown <ian0198@gmail.com>
2020-07-10 22:22:58 -0300

    Initial commit

---
+++ src/main/java/br/com/luizalabs/favourite/favouriteproducts/FavouriteProductsApplication.java
@@ -0,0 +1,13 @@
+package br.com.luizalabs.favourite.favouriteproducts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FavouriteProductsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FavouriteProductsApplication.class, args);
+	}
+
+}
