ca93e3a28c9651550f90cef91c87f5988ef9a16d
unknown <ian0198@gmail.com>
2020-07-15 21:22:43 -0300

    Feat: creation of service

---
+++ src/main/java/br/com/luizalabs/favourite/favouriteproducts/service/ClientService.java
@@ -0,0 +1,51 @@
+package br.com.luizalabs.favourite.favouriteproducts.service;
+
+import br.com.luizalabs.favourite.favouriteproducts.controller.dto.ClientDto;
+import br.com.luizalabs.favourite.favouriteproducts.model.Client;
+import br.com.luizalabs.favourite.favouriteproducts.repository.ClientRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@Service
+public class ClientService {
+
+
+    private final ClientRepository clientRepository;
+
+    public ClientService(ClientRepository clientRepository) {
+        this.clientRepository = clientRepository;
+    }
+
+    @Transactional
+    public Client create(Client client) {
+        return this.clientRepository.save(client);
+    }
+
+    @Transactional(readOnly = true)
+    public Optional<Client> findBy(Integer id) {
+        return this.clientRepository.findById(id);
+    }
+
+    @Transactional(readOnly = true)
+    public List<ClientDto> getAll() {
+        return clientRepository.findAll().stream().map(ClientDto::new).collect(Collectors.toList());
+    }
+
+    @Transactional
+    public Client update(Client newClient, Integer id) {
+        Client client = this.clientRepository.getOne(id);
+        client.setEmail(newClient.getEmail());
+        client.setName(newClient.getName());
+        return client;
+    }
+
+    @Transactional
+    public void delete(Integer id) {
+        Client client = this.clientRepository.getOne(id);
+        this.clientRepository.delete(client);
+    }
+}
