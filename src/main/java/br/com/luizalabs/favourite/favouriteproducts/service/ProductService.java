012a355b06e9edd54e6791b990c4c6730ef106fc
unknown <ian0198@gmail.com>
2020-07-16 13:16:23 -0300

    Refactor: returning null when product is not found

--- src/main/java/br/com/luizalabs/favourite/favouriteproducts/service/ProductService.java
+++ src/main/java/br/com/luizalabs/favourite/favouriteproducts/service/ProductService.java
@@ -3,6 +3,7 @@ package br.com.luizalabs.favourite.favouriteproducts.service;
 import br.com.luizalabs.favourite.favouriteproducts.model.Product;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.UUID;
@@ -18,6 +19,12 @@ public class ProductService {
 
     public Product getProduct(UUID id) {
         String url = "http://challenge-api.luizalabs.com/api/product/" + id.toString() + "/?";
-        return this.restTemplate.getForObject(url, Product.class);
+        Product product;
+        try {
+             product = this.restTemplate.getForObject(url, Product.class);
+        } catch (HttpClientErrorException e) {
+            return null;
+        }
+        return product;
     }
 }
