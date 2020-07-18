0683a894860a2a2a5ecb9c667a9c9c6cdeb39578
unknown <ian0198@gmail.com>
2020-07-18 00:03:43 -0300

    Feat: simple product caching

--- src/main/java/br/com/luizalabs/favourite/favouriteproducts/service/ProductService.java
+++ src/main/java/br/com/luizalabs/favourite/favouriteproducts/service/ProductService.java
@@ -6,12 +6,15 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.UUID;
 
 @Service
 public class ProductService {
 
     private final RestTemplate restTemplate;
+    private Map<UUID, Product> cachedProducts = new HashMap<>();
 
     public ProductService(RestTemplateBuilder restTemplateBuilder) {
         this.restTemplate = restTemplateBuilder.build();
@@ -19,12 +22,17 @@ public class ProductService {
 
     public Product getProduct(UUID id) {
         String url = "http://challenge-api.luizalabs.com/api/product/" + id.toString() + "/?";
+        if (cachedProducts.containsKey(id))
+            return cachedProducts.get(id);
         Product product;
         try {
-             product = this.restTemplate.getForObject(url, Product.class);
+            product = this.restTemplate.getForObject(url, Product.class);
         } catch (HttpClientErrorException e) {
             return null;
         }
+        cachedProducts.put(id, product);
         return product;
     }
+
+
 }
